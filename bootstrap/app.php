de0312a5e246c732928803c542c3cbe0693cdf9b
Paul Holt <paul-ho@lightflows.co.uk>
2025-08-04 00:12:20 +0100

    Update trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,15 +10,21 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+
+        // Trust proxies for HTTPS behind load balancer
+        $middleware->trustProxies(
+            headers: 31 // Trust all X-Forwarded headers
+        );
 
         $middleware->web(append: [
             ValidateCsrfToken::class,
