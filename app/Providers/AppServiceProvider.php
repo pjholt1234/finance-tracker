b74505d0425e5c532a1e67628ca95f8faa7c3420
Paul Holt <paul-ho@lightflows.co.uk>
2025-08-04 12:05:01 +0100

    Fix issue where app service provider boot code was overwritten

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\CsvSchema;
+use App\Models\Transaction;
+use App\Observers\TransactionObserver;
+use App\Policies\CsvSchemaPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Transaction::observe(TransactionObserver::class);
+
+        // Register policies
+        Gate::policy(CsvSchema::class, CsvSchemaPolicy::class);
+
         // Force HTTPS URL generation when FORCE_SCHEME is set
         if (config('app.env') === 'production' || env('FORCE_SCHEME') === 'https') {
             URL::forceScheme('https');
